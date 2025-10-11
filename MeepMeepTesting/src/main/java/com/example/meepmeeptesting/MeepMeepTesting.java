19403804bfe8c9e477779e4592d3208e2e62a0c1
hydra-student <hydradragonsftc@gmail.com>
2025-10-11 14:40:39 -0400

    Near Auto
    Add auto for near start position.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,41 +14,64 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        Pose2d Launch=new Pose2d(55, 15, Math.toRadians(-20));
-        Pose2d GPP_WP=new Pose2d(40, 30,Math.toRadians(90));
-        Pose2d GPP=new Pose2d(40,54,Math.toRadians(90));
-        Pose2d PGP_WP=new Pose2d(16, 30,Math.toRadians(90));
-        Pose2d PGP=new Pose2d(16,54,Math.toRadians(90));
-        Pose2d PPG_WP=new Pose2d(-7, 30,Math.toRadians(90));
-        Pose2d PPG=new Pose2d(-7,54,Math.toRadians(90));
+        Pose2d GPP_WP=new Pose2d(46, 30,Math.toRadians(90));
+        Pose2d GPP=new Pose2d(46,54,Math.toRadians(90));
+        Pose2d PGP_WP=new Pose2d(22, 30,Math.toRadians(90));
+        Pose2d PGP=new Pose2d(22,54,Math.toRadians(90));
+        Pose2d PPG_WP=new Pose2d(-4, 30,Math.toRadians(90));
+        Pose2d PPG=new Pose2d(-4,54,Math.toRadians(90));
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 15, Math.toRadians(0)))
-                .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(Launch, Math.toRadians(180))
-                .waitSeconds(1.5)
-                .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(GPP_WP, Math.toRadians(135))
-                .setTangent(Math.toRadians(90))
-                .splineToLinearHeading(GPP, Math.toRadians(90))
-                .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(Launch, Math.toRadians(0))
-                .waitSeconds(1.5)
-                .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(PGP_WP, Math.toRadians(135))
-                .setTangent(Math.toRadians(90))
-                .splineToLinearHeading(PGP, Math.toRadians(90))
-                .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(Launch, Math.toRadians(0))
-                .waitSeconds(1.5)
-                .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(PPG_WP, Math.toRadians(135))
-                .setTangent(Math.toRadians(90))
-                .splineToLinearHeading(PPG, Math.toRadians(90))
-                .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(Launch, Math.toRadians(0))
-                .waitSeconds(1.5)
-                .build());
-
+        if (false) {
+            Pose2d LaunchFar=new Pose2d(55, 15, Math.toRadians(-20));
+            myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 15, Math.toRadians(0)))
+                    .setTangent(Math.toRadians(180))
+                    .splineToLinearHeading(LaunchFar, Math.toRadians(180))
+                    .waitSeconds(1.5)
+                    .setTangent(Math.toRadians(180))
+                    .splineToLinearHeading(GPP_WP, Math.toRadians(135))
+                    .setTangent(Math.toRadians(90))
+                    .splineToLinearHeading(GPP, Math.toRadians(90))
+                    .setTangent(Math.toRadians(-90))
+                    .splineToLinearHeading(LaunchFar, Math.toRadians(0))
+                    .waitSeconds(1.5)
+                    .setTangent(Math.toRadians(180))
+                    .splineToLinearHeading(PGP_WP, Math.toRadians(135))
+                    .setTangent(Math.toRadians(90))
+                    .splineToLinearHeading(PGP, Math.toRadians(90))
+                    .setTangent(Math.toRadians(-90))
+                    .splineToLinearHeading(LaunchFar, Math.toRadians(0))
+                    .waitSeconds(1.5)
+                    .setTangent(Math.toRadians(180))
+                    .splineToLinearHeading(PPG_WP, Math.toRadians(135))
+                    .setTangent(Math.toRadians(90))
+                    .splineToLinearHeading(PPG, Math.toRadians(90))
+                    .setTangent(Math.toRadians(-90))
+                    .splineToLinearHeading(LaunchFar, Math.toRadians(0))
+                    .waitSeconds(1.5)
+                    .build());
+        } else {
+            Pose2d LaunchNear=new Pose2d(-25, 24, Math.toRadians(-40));
+            myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-54, 52, Math.toRadians(310)))
+                    .setTangent(Math.toRadians(315))
+                    .splineToLinearHeading(LaunchNear, Math.toRadians(315))
+                    .waitSeconds(1.5)
+                    .splineToLinearHeading(PPG_WP, Math.toRadians(90))
+                    .splineToLinearHeading(PPG, Math.toRadians(90))
+                    .setTangent(Math.toRadians(225))
+                    .splineToLinearHeading(LaunchNear, Math.toRadians(-90))
+                    .splineToLinearHeading(PGP_WP, Math.toRadians(90))
+                    .splineToLinearHeading(PGP, Math.toRadians(90))
+                    .setTangent(Math.toRadians(225))
+                    .splineToLinearHeading(LaunchNear, Math.toRadians(225))
+                    .waitSeconds(1.5)
+                    .setTangent(Math.toRadians(25))
+                    .splineToLinearHeading(GPP_WP, Math.toRadians(90))
+                    .splineToLinearHeading(GPP, Math.toRadians(90))
+                    .setTangent(Math.toRadians(225))
+                    .splineToLinearHeading(LaunchNear, Math.toRadians(180))
+                    .waitSeconds(1.5)
+                    .build());
+        }
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/HydrAuto2.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.opMode;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "HydrAuto2", preselectTeleOp = "HyDrive_Red")
+public class HydrAuto2 extends HydrAuto {
+
+    public HydrAuto2() {
+        mBeginPose = new Pose2d(-54, 52, Math.toRadians(310));
+    }
+
+    @Override
+    protected SequentialAction CreateAuto() {
+
+        Pose2d GPP_WP=new Pose2d(46, 30,Math.toRadians(90));
+        Pose2d GPP=new Pose2d(46,54,Math.toRadians(90));
+        Pose2d PGP_WP=new Pose2d(22, 30,Math.toRadians(90));
+        Pose2d PGP=new Pose2d(22,54,Math.toRadians(90));
+        Pose2d PPG_WP=new Pose2d(-4, 30,Math.toRadians(90));
+        Pose2d PPG=new Pose2d(-4,54,Math.toRadians(90));
+        Pose2d LaunchNear=new Pose2d(-25, 24, Math.toRadians(-40));
+
+        Action driveToLaunch1 = mDrive.actionBuilder(mBeginPose)
+                .setTangent(Math.toRadians(315))
+                .splineToLinearHeading(LaunchNear, Math.toRadians(315))
+                .waitSeconds(1.5)
+                .splineToLinearHeading(PPG_WP, Math.toRadians(90))
+                .splineToLinearHeading(PPG, Math.toRadians(90))
+                .setTangent(Math.toRadians(225))
+                .splineToLinearHeading(LaunchNear, Math.toRadians(-90))
+                .waitSeconds(1.5)
+                .splineToLinearHeading(PGP_WP, Math.toRadians(90))
+                .splineToLinearHeading(PGP, Math.toRadians(90))
+                .setTangent(Math.toRadians(225))
+                .splineToLinearHeading(LaunchNear, Math.toRadians(225))
+                .waitSeconds(1.5)
+                .setTangent(Math.toRadians(25))
+                .splineToLinearHeading(GPP_WP, Math.toRadians(90))
+                .splineToLinearHeading(GPP, Math.toRadians(90))
+                .setTangent(Math.toRadians(225))
+                .splineToLinearHeading(LaunchNear, Math.toRadians(180))
+                .waitSeconds(1.5)
+                .build();
+
+        return new SequentialAction(driveToLaunch1);
+    }
+}
