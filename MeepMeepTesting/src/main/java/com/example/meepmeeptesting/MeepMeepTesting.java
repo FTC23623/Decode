8ab36bfd40411466430e8ada5dc02854a679e7c9
hydra-student <hydradragonsftc@gmail.com>
2025-10-11 11:06:52 -0400

    Auto simulation
    Simulate picking up and launching 12 artifacts on red

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,16 +13,38 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
-
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        Pose2d Launch=new Pose2d(55, 15, Math.toRadians(-20));
+        Pose2d GPP_WP=new Pose2d(35, 30,Math.toRadians(90));
+Pose2d GPP=new Pose2d(35,48,Math.toRadians(90));
+        Pose2d PGP_WP=new Pose2d(11, 30,Math.toRadians(90));
+        Pose2d PGP=new Pose2d(11,48,Math.toRadians(90));
+        Pose2d PPG_WP=new Pose2d(-12, 30,Math.toRadians(90));
+        Pose2d PPG=new Pose2d(-12,48,Math.toRadians(90));
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 15, Math.toRadians(0)))
+                .setTangent(Math.toRadians(180))
+                .splineToLinearHeading(Launch, Math.toRadians(180))
+                .waitSeconds(1.5)
+                .setTangent(Math.toRadians(180))
+                .splineToLinearHeading(GPP_WP, Math.toRadians(135))
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(GPP, Math.toRadians(90))
+                .setTangent(Math.toRadians(-90))
+                .splineToLinearHeading(Launch, Math.toRadians(0))
+                .waitSeconds(1.5)
+                .setTangent(Math.toRadians(180))
+                .splineToLinearHeading(PGP_WP, Math.toRadians(135))
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(PGP, Math.toRadians(90))
+                .setTangent(Math.toRadians(-90))
+                .splineToLinearHeading(Launch, Math.toRadians(0))
+                .waitSeconds(1.5)
+                .setTangent(Math.toRadians(180))
+                .splineToLinearHeading(PPG_WP, Math.toRadians(135))
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(PPG, Math.toRadians(90))
+                .setTangent(Math.toRadians(-90))
+                .splineToLinearHeading(Launch, Math.toRadians(0))
+                .waitSeconds(1.5)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
