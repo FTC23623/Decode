472002f9ce4914e152e064b983311de8536b213b
bkleinh <74694427+bkleinh@users.noreply.github.com>
2025-11-01 15:59:59 -0400

    Filter results for april tags to avoid false positives

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightVision.java
@@ -7,16 +7,20 @@ import org.firstinspires.ftc.teamcode.objects.Vision;
 import org.firstinspires.ftc.teamcode.objects.LimelightVisionResult;
 import org.firstinspires.ftc.teamcode.objects.VisionResult;
 import org.firstinspires.ftc.teamcode.types.Constants;
+import org.firstinspires.ftc.teamcode.types.DecodeAprilTag;
 import org.firstinspires.ftc.teamcode.types.VisionMode;
+import java.util.ArrayList;
 
 public class LimelightVision implements Vision {
     private Limelight3A ll;
     private LLResult result;
     private boolean isInit;
+    private ArrayList<DecodeAprilTag> targetTags;
 
     public LimelightVision(HydraOpMode opMode) {
         ll = opMode.mHardwareMap.get(Limelight3A.class, "limelight");
         isInit = false;
+        targetTags = new ArrayList<>();
     }
 
     @Override
@@ -37,7 +41,12 @@ public class LimelightVision implements Vision {
     @Override
     public VisionResult GetResult() {
         if (result != null && result.isValid()) {
-            return new LimelightVisionResult(result);
+            VisionResult ret = new LimelightVisionResult(result);
+            if (targetTags.contains(ret.GetTagClass())) {
+                return ret;
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
@@ -45,16 +54,23 @@ public class LimelightVision implements Vision {
 
     @Override
     public void SetMode(VisionMode mode) {
+        targetTags.clear();
+        result = null;
         switch (mode) {
             case VisionMode_Obelisk:
+                targetTags.add(DecodeAprilTag.DecodeTag_Obelisk_GPP);
+                targetTags.add(DecodeAprilTag.DecodeTag_Obelisk_PGP);
+                targetTags.add(DecodeAprilTag.DecodeTag_Obelisk_PPG);
                 ll.pipelineSwitch(0);
                 ll.start();
                 break;
             case VisionMode_BlueGoal:
+                targetTags.add(DecodeAprilTag.DecodeTag_BlueGoal);
                 ll.pipelineSwitch(1);
                 ll.start();
                 break;
             case VisionMode_RedGoal:
+                targetTags.add(DecodeAprilTag.DecodeTag_RedGoal);
                 ll.pipelineSwitch(2);
                 ll.start();
                 break;
