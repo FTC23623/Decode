c46964057aac997c70acf72a844f6bb6df035c5f
bkleinh <74694427+bkleinh@users.noreply.github.com>
2025-11-18 20:30:30 -0500

    Slow down slow drive speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/types/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/types/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     public static final double trgBtnThresh = 0.05;
     public static final double driveBoosted = 1;
     public static final double driveNormal = 0.95;
-    public static final double driveSlow = 0.5;
+    public static final double driveSlow = 0.25;
     public static final double contServoOff = 0.5;
     public static final double contServoForward = 1.0;
     public static final double contServoBackward = 0.05;
