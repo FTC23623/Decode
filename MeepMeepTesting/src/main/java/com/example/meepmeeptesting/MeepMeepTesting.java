508d6eb405ccaa81a764d9542f4dd5e75cf4be4c
hydra-student <hydradragonsftc@gmail.com>
2026-02-18 18:28:29 -0500

    Update MeepMeepTesting.java
    Bring changes from opmode

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -107,9 +107,9 @@ public class MeepMeepTesting {
         // FlipPose and FlipTangent auto adjust for blue
         Pose2d Launch = FlipPose(55, 15, -20, flip);
         Pose2d GPP_WP = FlipPose(34, 30, 90, flip);
-        Pose2d GPP = FlipPose(34, 56, 90, flip);
+        Pose2d GPP = FlipPose(34, 58, 90, flip);
         Pose2d PGP_WP = FlipPose(12, 30, 90, flip);
-        Pose2d PGP = FlipPose(12, 56, 90, flip);
+        Pose2d PGP = FlipPose(12, 58, 90, flip);
         Pose2d PPG_WP = FlipPose(-12, 35, 90, flip);
         Pose2d PPG = FlipPose(-12, 48, 90, flip);
         Pose2d End = FlipPose(30, 15, 0, flip);
