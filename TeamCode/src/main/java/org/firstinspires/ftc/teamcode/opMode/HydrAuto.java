5c771dd8719177ca64742a8db7b373fce828e2e7
bkleinh <74694427+bkleinh@users.noreply.github.com>
2026-04-19 14:34:45 -0400

    Adjust loading zone position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/HydrAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/HydrAuto.java
@@ -152,7 +152,7 @@ public abstract class HydrAuto extends OpMode_Base {
     }
 
     protected SequentialAction LoadingZoneSequence(Pose2d LaunchPos, boolean driveToLaunch, boolean reject, Pose2d StartPos) {
-        Pose2d LoadingZone = FlipPose(64,54,90);
+        Pose2d LoadingZone = FlipPose(64,53,90);
         Pose2d LoadingZone_WP= FlipPose(59, 40, 90);
         Pose2d LoadingZone_WP2= FlipPose(59, 50, 90);
         Pose2d Slowdown_Pose = Waypoint(StartPos, LoadingZone, 0.75);
